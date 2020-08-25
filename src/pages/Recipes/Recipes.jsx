d9f7123910db17ddb2c4254d3f0d2cbb0b8143c4
lucasbpro <lucasbpro@gmail.com>
2020-08-24 23:13:26 -0300

    feat: updated code to use heroku web API

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 //import Loading from '../../components/Loading';
 
+import {getAllRecipes, getAllMaterials} from '../../services';
+
 import RecipeFilter from '../../components/RecipeFilter'
 import RecipesTable from '../../containers/RecipesTable';
 import './Recipes.scss';
@@ -10,48 +12,18 @@ import {filterListByText} from '../../utils/filters'
 
 const Recipes = () => {
 
-  const URL_RECIPES = "http://localhost:8000/recipes";
-  const URL_MATERIALS = "http://localhost:8000/raw_materials";
-
   const [recipesList, setRecipes] = useState([]);
   const [allMaterials, setAllMaterials] = useState([]);
-
   const [filteredRecipes, setFilteredRecipes] = useState([]);
   const [isFilterApplied, setFilterApplied] = useState(false);
 
-  useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      fetch(URL_RECIPES)
-        .then(async (response) => {
-          if (response.ok) {
-            const resposta = await response.json();
-            setRecipes(resposta);
-            return;
-          }
-          throw new Error(`Error when communicating with server at ${URL_RECIPES}`);
-        });
-    }
-  }, [])
-
-  useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      fetch(URL_MATERIALS)
-        .then(async (response) => {
-          if (response.ok) {
-            const resposta = await response.json();
-            setAllMaterials(resposta);
-            return;
-          }
-          throw new Error(`Error when communicating with server at ${URL_MATERIALS}`);
-        });
-    }
-  }, [])
+  useEffect(() => getAllRecipes().then(resposta => setRecipes(resposta)), [])
+  useEffect(() => getAllMaterials().then(resposta => setAllMaterials(resposta)), [])
 
   const handleFilter = (event) => {
       setFilteredRecipes(filterListByText(recipesList, event.target.value))
       setFilterApplied(true)
   }
-
 
   return (
     <div className="container">
--- src/services/index.js
+++ src/services/index.js
@@ -1,25 +1,24 @@
-const request = require('request-promise');
+const URL_API = "https://essentialis-server.herokuapp.com";
+const URL_RECIPES = `${URL_API}/recipes`;
+const URL_MATERIALS = `${URL_API}/raw_materials`;
 
-// Coloque aqui sua Chave de API
-const apiKey = '86c8930f-d247-4887-9bde-7b11f9caf275-31231306-a0d0-4de9-8d9c-ed6351c0e6a6';
+async function get(URL){
+  return fetch(URL).then(async (response) => {
+      if (response.ok)
+        return await response.json();
+      throw new Error(`Error when communicating with server at ${URL}`);
+    });
+}
 
-/**
- * Dado um CNPJ, consulta a Receita Federal e adquire as informações
- * da inscrição bem como CNAEs e sócios 
- * @param { string } taxId - CNPJ a ser consultado
- */
-async function getCompany(taxId) {
-  const cnpjaHost = 'https://api.cnpja.com.br';
-  const cleanTaxId = taxId ? taxId.toString().replace(/\D+/g, '') : null;
+async function getAllRecipes(){
+  return get(URL_RECIPES);
+}
 
-  const company = await request({
-    uri: `${cnpjaHost}/companies/${cleanTaxId}`,
-    headers: { 'Authorization': apiKey },
-    json: true,
-  });
-  return company;
+async function getAllMaterials(){
+  return get(URL_MATERIALS);
 }
 
 export {
-    getCompany
+    getAllRecipes,
+    getAllMaterials
 }
