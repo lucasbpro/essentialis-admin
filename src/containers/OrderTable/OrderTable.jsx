563c579cb8be98d66655c56e70906225aac3cd80
lucasbpro <lucasbpro@gmail.com>
2020-09-13 20:51:06 -0300

    feat: orders page initial version

--- src/containers/OrderTable/OrderTable.jsx
+++ src/containers/OrderTable/OrderTable.jsx
@@ -1,47 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import OrderItem from '../../components/OrderItem';
 import Table from 'react-bootstrap/Table'
 
-import {getRecipeById} from '../../services'
-
 const OrderTable = ({ordersList}) => { 
 
-      const [orderListComplete, setOrderList] = useState([]);
-      const [loading, setLoading] = useState(true);
-
-      const addProductDescription = (ordersList) => {
-         return ordersList.map(order => {
-            const orderDescription = getRecipeById(order.product_id).then(item => item.description);
-            return orderDescription.then(resposta => {
-               return {...order, "productDescription": resposta}
-            });
-         })
-
-         /*
-         let orderListComplete = [];
-
-         if(ordersList.length>0)
-           for(let i=1; i<ordersList.length; i++){
-               orderListComplete.push({...ordersList[i], 
-                                       "productDescription": getRecipeById(ordersList[i].product_id)});
-           }
-
-         return orderListComplete; 
-         */
-      }
-
-      useEffect(() => {
-         setOrderList(addProductDescription(ordersList))
-      },[ordersList]);
-
-      useEffect(() => {
-         console.log(orderListComplete)
-         setLoading(false);
-      },[ordersList, orderListComplete]);
-
-      if (orderListComplete.length===0 || loading)
-         return null;
-      else return (
+   if (ordersList.length===0)
+      return null;
+   else return (
             <Table striped bordered hover>
                <thead>
                   <tr>
@@ -53,7 +18,7 @@ const OrderTable = ({ordersList}) => {
                </thead>
 
                <tbody>
-                  {ordersList && orderListComplete.map((order, index)  => {
+                  {ordersList && ordersList.map((order, index)  => {
 
                      return <OrderItem key={index} 
                                        index={index}
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -4,30 +4,53 @@ import React, {useState, useEffect} from 'react';
 import OrderFilter from '../../components/OrderFilter'
 import OrderTable  from '../../containers/OrderTable';
 import {filterOrdersByStatus, sortOrdersByDate} from '../../utils/filters'
-import {getAllOrders} from '../../services';
+import {getAllOrders, getAllRecipes} from '../../services';
 
 const Orders = () => {
 
-  const [orderList, setOrders] = useState([]);
+  const [orderList, setOrderList] = useState([]);
+  const [orderListComplete, setOrderListComplete] = useState([]);
+
+  const [productList, setProductList] = useState([]);
+  const [fetched, setFetched] = useState(false);
+
   const [filteredOrders, setFilteredOrders] = useState([]);
   const [isFilterApplied, setFilterApplied] = useState(false);
 
   useEffect(() => {
-    getAllOrders().then(out => sortOrdersByDate(out)).then(out=>setOrders(out));
-  }, [])
+    getAllOrders().then(resposta =>setOrderList(resposta));
+  },[]);
+
+  useEffect(() => {
+    getAllRecipes().then(resposta =>setProductList(resposta));
+  },[]);
+
+  useEffect(() => {
+    setOrderListComplete(sortOrdersByDate(orderList.map(order => {
+        const product = productList.find(item => item.id === order.product_id);
+        return {...order, 
+                "productDescription": product? product.description : ""}
+    })))
+    setFetched(true);
+  },[orderList, productList]);
 
   const handleFilter = (event) => {
-      setFilteredOrders(filterOrdersByStatus(orderList, event.target.value))
-      setFilterApplied(true)
+      setFilteredOrders(filterOrdersByStatus(orderListComplete, event.target.value))
+      setFilterApplied(true);
   }
 
-  return (
-    <div className="container">
+     
+  if (fetched)
+    return (
+      <div className="container">
         <OrderFilter handleFilter={handleFilter}/>
         {(isFilterApplied && filteredOrders.length===0) && <h3> O filtro não retornou resultados </h3>}
         {isFilterApplied && <OrderTable ordersList={filteredOrders}/>}
-    </div>
-  );
+      </div>
+    );
+  else 
+    return <h1> Carregando... </h1>;
+
 };
 
 export default Orders;
