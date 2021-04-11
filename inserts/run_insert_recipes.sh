49bdc57b666075b12c6f9fd09210b4de7f04b722
lucasbpro <lucasbpro@gmail.com>
2021-04-10 21:07:16 -0300

    feat: merged with main branch

---
+++ inserts/run_insert_recipes.sh
@@ -0,0 +1,83 @@
+echo "Starting script..."
+
+node insert_recipes.js 0
+sleep 10
+node insert_recipes.js	1
+sleep 10
+node insert_recipes.js	2
+sleep 10
+node insert_recipes.js	3
+sleep 10
+node insert_recipes.js	4
+sleep 10
+node insert_recipes.js	5
+sleep 10
+node insert_recipes.js	6
+sleep 10
+node insert_recipes.js	7
+sleep 10
+node insert_recipes.js	8
+sleep 10
+node insert_recipes.js	9
+sleep 10
+node insert_recipes.js	10
+sleep 10
+node insert_recipes.js	11
+sleep 10
+node insert_recipes.js	12
+sleep 10
+node insert_recipes.js	13
+sleep 10
+node insert_recipes.js	14
+sleep 10
+node insert_recipes.js	15
+sleep 10
+node insert_recipes.js	16
+sleep 10
+node insert_recipes.js	17
+sleep 10
+node insert_recipes.js	18
+sleep 10
+node insert_recipes.js	19
+sleep 10
+node insert_recipes.js	20
+sleep 10
+node insert_recipes.js	21
+sleep 10
+node insert_recipes.js	22
+sleep 10
+node insert_recipes.js	23
+sleep 10
+node insert_recipes.js	24
+sleep 10
+node insert_recipes.js	25
+sleep 10
+node insert_recipes.js	26
+sleep 10
+node insert_recipes.js	27
+sleep 10
+node insert_recipes.js	28
+sleep 10
+node insert_recipes.js	29
+sleep 10
+node insert_recipes.js	30
+sleep 10
+node insert_recipes.js	31
+sleep 10
+node insert_recipes.js	32
+sleep 10
+node insert_recipes.js	33
+sleep 10
+node insert_recipes.js	34
+sleep 10
+node insert_recipes.js	35
+sleep 10
+node insert_recipes.js	36
+sleep 10
+node insert_recipes.js	37
+sleep 10
+node insert_recipes.js	38
+sleep 10
+node insert_recipes.js	39
+sleep 10
+node insert_recipes.js	40
