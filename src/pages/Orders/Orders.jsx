7966510a0377de579190e25a5674092f5a4a18b4
lucasbpro <lucasbpro@gmail.com>
2021-04-12 23:22:55 -0300

    feat: Changed Orders page to consume global state, worked on Login

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,58 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Redirect } from 'react-router-dom';
+
 //import Loading from '../../components/Loading';
 import OrderFilter from '../../components/OrderFilter'
 import OrderTable  from '../../containers/OrderTable';
 import {filterOrdersByText, sortOrdersByDate} from '../../utils/filters'
-import {getAllOrders, getAllRecipes, getAllCustomers} from '../../services';
 import { useSelector } from 'react-redux';
 
 const Orders = () => {
 
   const userLogged = useSelector(state => state.isUserLogged);
+  const productList = useSelector(state => state.recipeList);
+  const customerList = useSelector(state => state.customerList);
+  const orderList = useSelector(state => state.orderList);
 
-  const [orderList, setOrderList] = useState([]);
-  const [orderListComplete, setOrderListComplete] = useState([]);
-
-  const [productList, setProductList] = useState([]);
-  const [customerList, setCustomerList] = useState([]);
-  const [fetched, setFetched] = useState(false);
-
-  const [filteredOrders, setFilteredOrders] = useState([]);
   const [isFilterApplied, setFilterApplied] = useState(false);
 
-  useEffect(() => {
-      getAllOrders().then(resposta =>setOrderList(resposta));
-      setFilteredOrders(orderList)
-  },[orderList]);
+  const orderListComplete = sortOrdersByDate(orderList.map(order => {
+      const product = productList.find(item => item.id === order.product_id);
+      const customer = customerList.find(item => item.id === order.customer_id);
+      return {...order, 
+              "productDescription": product? product.description : "",
+              "customerName" : customer? customer.name: ""
+            }
+  }));
 
-  useEffect(() => {
-      getAllRecipes().then(resposta =>setProductList(resposta));
-  },[]);
-
-  useEffect(() => {
-      getAllCustomers().then(resposta =>setCustomerList(resposta));
-  },[]);
-
-  useEffect(() => {
-      setOrderListComplete(sortOrdersByDate(orderList.map(order => {
-          const product = productList.find(item => item.id === order.product_id);
-          const customer = customerList.find(item => item.id === order.customer_id);
-          return {...order, 
-                  "productDescription": product? product.description : "",
-                  "customerName" : customer? customer.name: ""
-                }
-      })));
-
-      setFilteredOrders(orderListComplete);
+  const [filteredOrders, setFilteredOrders] = useState(orderListComplete);
       
-  },[orderList, orderListComplete, productList, customerList]);
-
-  useEffect(() => {
-      setFilteredOrders(orderListComplete);
-      setFetched(true);
-  },[orderListComplete]);
-
   const handleFilter = (event) => {
       setFilteredOrders(filterOrdersByText(orderListComplete, event.target.value))
       setFilterApplied(true);
@@ -60,17 +34,13 @@ const Orders = () => {
 
   if(!userLogged)
     return <Redirect to='/login'/>
-  else if (fetched)
-    return (
+  else return (
       <div className="container">
         <OrderFilter handleFilter={handleFilter}/>
         <OrderTable ordersList={filteredOrders}/>
         {(isFilterApplied && filteredOrders.length===0) && <h3> O filtro não retornou resultados </h3>}
       </div>
     );
-  else 
-    return <h1> Carregando... </h1>;
-
 };
 
 export default Orders;
