c81fa8d3831f942809156c2b60e91330ca4ef6ab
lucasbpro <lucasbpro@gmail.com>
2021-05-02 15:23:58 -0300

    feat: added supplier name to raw_materials resource

--- data/csv_files/csv2json.py
+++ data/csv_files/csv2json.py
@@ -1,15 +1,18 @@
-import csv, json
+import csv, json, sys
 
-csvFilePath = 'test.csv'
-jsonFilePath = 'json_file_name.json'
+nargs = len(sys.argv)
+if(nargs!= 3):
+    print("Script needs two arguments: input and output file names. {} parameters provided.".format(nargs-1))
+else:
+    arguments = sys.argv
+    csvFilePath = arguments[1]
+    jsonFilePath = arguments[2]
 
-data = {}
-id = 1
-with open(csvFilePath) as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for rows in csvReader:
-        data[id] = rows
-        id = id + 1
+    data = []
+    with open(csvFilePath) as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for rows in csvReader:
+            data.append(rows)
 
-with open(jsonFilePath, 'w') as jsonFile:
-    jsonFile.write(json.dumps(data, indent=4))
+    with open(jsonFilePath, 'w') as jsonFile:
+        jsonFile.write(json.dumps(data, indent=4))
