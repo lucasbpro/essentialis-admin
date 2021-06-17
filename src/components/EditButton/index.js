0e2084263abff836e5c16bd915d6c961dc2449de
lucasbpro <lucasbpro@gmail.com>
2021-06-17 17:19:49 -0300

    feat: Recipes inserts and modifications in Recipe page (Edit,Delete)

---
+++ src/components/EditButton/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { PencilButton } from './styles'
+import { ReactComponent as PencilIcon } from "../../assets/img/edit_icon.svg";
+
+const EditButton = ({onClick}) => {
+    return(
+        <PencilButton className="trash-button"> 
+            <PencilIcon onClick={onClick}/> 
+        </PencilButton>
+    )
+}
+
+export default EditButton;
---
+++ src/components/EditButton/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const PencilButton = styled.button`
+    background-color: white;
+    width: auto;
+    border-radius: 0;
+    box-shadow: none;
+    outline: none;
+    height: fit-content;
+    svg {
+        height: 15px
+    }
+`;
